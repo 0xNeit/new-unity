2c40e6ce1a9e165d3fed6aaecb8c33109b70e80c
Nate <89227631+natedotv2@users.noreply.github.com>
2023-08-10 07:18:22 +0100

    new setup again

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import inject from '@rollup/plugin-inject';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgrPlugin from 'vite-plugin-svgr';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.tsx',
+    coverage: {
+      reporter: ['text', 'html', 'json-summary', 'json'],
+      exclude: ['node_modules/', 'src/setupTests.tsx'],
+    },
+  },
+  server: {
+    open: true,
+    port: 4000,
+  },
+  define: {
+    'process.env': {}
+  },
+}));
